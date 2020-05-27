25fc1a951b65ff6c864c71a7dde1b3b70cec4745
sfofana <58987175+sfofana@users.noreply.github.com>
2020-05-27 13:26:37 -0400

    update controller exception

--- ImgRecApp/src/main/java/com/sfofana/app/controller/UserController.java
+++ ImgRecApp/src/main/java/com/sfofana/app/controller/UserController.java
@@ -41,17 +41,21 @@ public class UserController {
 	
 	@PostMapping("compare")
 	public Compare compareFaces(@RequestBody Compare faces) throws Exception {
-		if(!service.tokenAuthenticated(getToken())) {
+		try {
+			service.tokenAuthenticated(getToken());
+		} catch (Exception e) {
 			throw new Exception("Access Denied");
-		}
+		}		
 		return service.compareFacesResults(faces);
 	}
 	
 	@PostMapping("upload/{name}")
 	public Upload uploadImage(@PathVariable("name") String fileName, @RequestParam("file") MultipartFile file) throws Exception {
-		if(!service.tokenAuthenticated(getToken())) {
+		try {
+			service.tokenAuthenticated(getToken());
+		} catch (Exception e) {
 			throw new Exception("Access Denied");
-		}
+		}	
 		return service.processImageUpload(fileName, file);
 	}
 	
